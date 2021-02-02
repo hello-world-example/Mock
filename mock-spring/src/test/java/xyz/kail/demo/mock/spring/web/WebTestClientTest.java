d8a5cfa69a9c31647c60e5758d0004209fbcf02c
kaibin.yang <ykb553@163.com>
2021-02-02 15:45:50 +0800

    noting

--- mock-spring/src/test/java/xyz/kail/demo/mock/spring/web/WebTestClientTest.java
+++ mock-spring/src/test/java/xyz/kail/demo/mock/spring/web/WebTestClientTest.java
@@ -2,16 +2,12 @@ package xyz.kail.demo.mock.spring.web;
 
 import org.hamcrest.Matchers;
 import org.junit.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.web.context.WebApplicationContext;
 import xyz.kail.demo.mock.controller.HelloController;
 
 import javax.annotation.Resource;
 
-    @SpringJUnitConfig(WebConfig.class)
 public class WebTestClientTest {
 
     @Resource
@@ -27,13 +23,4 @@ public class WebTestClientTest {
                 .expectBody().jsonPath("$.p1").value(Matchers.equalTo("P"));
     }
 
-    @Test
-    public void bindToApplicationContext() {
-        final WebTestClient testClient = WebTestClient.bindToApplicationContext(wac).build();
-        testClient.get()
-                .uri("/demo/hello/say/{world}?p1=P", "W")
-                .exchange()
-                .expectStatus().isOk()
-                .expectBody().jsonPath("$.p1").value(Matchers.equalTo("P"));
-    }
 }
